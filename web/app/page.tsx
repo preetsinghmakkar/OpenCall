618618db99887b6d95e60d92939c8deab6cb5400
preetsinghmakkar <preetsinghmakkar154@gmail.com>
2026-01-12 15:07:32 +0530

    Frontend Initialized

---
+++ web/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home  = () => {
+  return (
+    <div>
+      <h1 className='bg-red-200'>Welcome to the Home Page</h1>
+    </div>
+  )
+}
+
+export default Home 
